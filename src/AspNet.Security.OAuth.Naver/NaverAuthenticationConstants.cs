4512ae0dd8e5a4f612da27093d0430f9dd6987d6
kasumielf <kasumielf@lof.kr>
2021-12-24 14:51:22 +0900

    Add Naver authentication provider.

---
+++ src/AspNet.Security.OAuth.Naver/NaverAuthenticationConstants.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Naver;
+
+public static class NaverAuthenticationConstants
+{
+    public static class Claims
+    {
+        /// <summary>
+        /// The claim for the user's age.
+        /// </summary>
+        public const string Age = "urn:naver:age";
+
+        /// <summary>
+        /// The claim for the user's year of birth.
+        /// </summary>
+        public const string YearOfBirth = "urn:naver:birthyear";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Naver/NaverAuthenticationOptions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using static AspNet.Security.OAuth.Naver.NaverAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Naver;
+
+public class NaverAuthenticationOptions : OAuthOptions
+{
+    public NaverAuthenticationOptions()
+    {
+        ClaimsIssuer = NaverAuthenticationDefaults.Issuser;
+        CallbackPath = NaverAuthenticationDefaults.CallbackPath;
+
+        AuthorizationEndpoint = NaverAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = NaverAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = NaverAuthenticationDefaults.UserInformationEndpoint;
+
+        ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "response", "id");
+        ClaimActions.MapJsonKey(ClaimTypes.Name, "response", "name");
+        ClaimActions.MapJsonKey(ClaimTypes.Email, "response", "email");
+        ClaimActions.MapJsonKey(ClaimTypes.Gender, "response", "gender");
+        ClaimActions.MapJsonKey(Claims.Age, "response", "age");
+        ClaimActions.MapJsonKey(ClaimTypes.DateOfBirth, "response", "birthday");
+        ClaimActions.MapJsonKey(Claims.YearOfBirth, "response", "birthyear");
+        ClaimActions.MapJsonKey(ClaimTypes.MobilePhone, "response", "mobile");
+    }
+}
