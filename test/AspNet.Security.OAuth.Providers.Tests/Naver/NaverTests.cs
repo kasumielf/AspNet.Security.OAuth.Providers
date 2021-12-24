43cf872dccd74d4c9fc59f41379d10cb7c215962
kasumielf <kasumielf@lof.kr>
2021-12-24 14:51:43 +0900

    Add Naver authentication test.

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Naver/NaverTests.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using static AspNet.Security.OAuth.Naver.NaverAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Naver;
+
+public class NaverTests : OAuthTests<NaverAuthenticationOptions>
+{
+    public NaverTests(ITestOutputHelper outputHelper)
+    {
+        OutputHelper = outputHelper;
+    }
+
+    public override string DefaultScheme => NaverAuthenticationDefaults.AuthenticationScheme;
+
+    protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+    {
+        builder.AddNaver(options => ConfigureDefaults(builder, options));
+    }
+
+    [Theory]
+    [InlineData(ClaimTypes.NameIdentifier, "NpoHyTWpCXoCgGLV3Ew")]
+    [InlineData(ClaimTypes.Name, "kasumielf")]
+    [InlineData(ClaimTypes.Email, "example@naver.com")]
+    [InlineData(ClaimTypes.DateOfBirth, "0828")]
+    [InlineData(ClaimTypes.Gender, "male")]
+    [InlineData(ClaimTypes.MobilePhone, "+821012341234")]
+    public async Task Can_Sign_In_Using_Naver(string claimType, string claimValue)
+    {
+        // Arrange
+        using var server = CreateTestServer();
+
+        // Act
+        var claims = await AuthenticateUserAsync(server);
+
+        // Assert
+        AssertClaim(claims, claimType, claimValue);
+    }
+}
